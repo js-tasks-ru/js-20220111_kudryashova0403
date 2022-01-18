39793f0f59ee424689d71945f42ac95eb1b730f5
akudryashova <akudryashova@diasoft.ru>
2022-01-18 15:58:56 +0300

    2 задача первый пункт

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,15 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  if (param === 'desc') {
+    return arr.sort((a, b) =>
+      b.localeCompare(a, ['ru', 'en'], { caseFirst: 'upper' })
+    );
+  } else {
+    return arr.sort((a, b) =>
+      a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'})
+    );
+  }
 }
+console.log(sortStrings(['bc', 'Nya', 'aa', 'Кя', 'br', 'Tu', 'аст'], 'desc'));
+console.log(sortStrings(['ac', 'Nya', 'aa', 'Кя', 'br', 'Tu', 'август']));
